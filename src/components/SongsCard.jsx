91e1062715b97ab5148997b161a43c238b5ee9cd
Seu nome <senamatheus.job@gmail.com>
2022-02-23 09:48:10 -0300

    cards para musicas e funçao de adicionar aos favoritos

--- src/components/SongsCard.jsx
+++ src/components/SongsCard.jsx
@@ -1,7 +1,50 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import PropTypes from 'prop-types';
+import TunesContext from '../context/TunesContext';
 
-export default function SongsCard() {
+export default function SongsCard(props) {
+  const { trackName, previewUrl, trackId } = props;
+  // const [teste, setTeste] = useState('');
+  const [checked, setChecked] = useState(true);
+  const { sendFavoriteMusic,
+    favoriteMusics,
+    setFavoriteMusics,
+  } = useContext(TunesContext);
+
+  const submitMusic = () => {
+    const newMusic = {
+      trackName,
+      previewUrl,
+      trackId,
+    };
+    setChecked((prevState) => !prevState);
+
+    if (checked) {
+      sendFavoriteMusic(newMusic);
+    } else {
+      const result = favoriteMusics.filter((music) => music.trackId !== trackId);
+      setFavoriteMusics(result);
+    }
+  };
+
+  console.log(favoriteMusics);
+
   return (
-    <div>SongsCard</div>
+    <div className="MusicCard">
+      <h4>{trackName}</h4>
+      <audio data-testid="audio-component" src={ previewUrl } controls>
+        <track kind="captions" />
+      </audio>
+      <input
+        type="checkbox"
+        onChange={ submitMusic }
+      />
+    </div>
   );
 }
+
+SongsCard.propTypes = {
+  trackName: PropTypes.string.isRequired,
+  previewUrl: PropTypes.string.isRequired,
+  trackId: PropTypes.number.isRequired,
+};
